b68b099349de2ed3a64901563766e96f514801cb
avanishkumar5035 <avanishkumar5035@gmail.com>
2025-12-21 21:41:24 +0530

    update

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+    role:{
+        type: String,
+        default: "user",
+    },
+    subscription: [{
+        type: mongoose.Schema.Types.ObjectId,
+         ref: "course",
+    }]
+
+});
