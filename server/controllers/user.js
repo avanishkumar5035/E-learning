4ca17d61a347f6340c31b62a61f8e04111bda9fc
avanishkumar5035 <avanishkumar5035@gmail.com>
2025-12-22 20:14:30 +0530

    22 dec E-learning project commit

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,47 @@
+import { User } from "../models/user.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+
 export  const register = async(req,res) =>{
     try {
+        const { email, name, password } = req.body
+
+        let user = await User.findOne({email});
+
+
+        if (user) return res.status(400).json({
+            message: "User Already exists",
+        });
+
+        const hashPassword = await bcrypt.hash(password, 10 )
+
+        user = {
+            name,
+            email,
+            password: hashPassword,
+        }
+
+
+        const otp = Math.floor(Math.random() * 1000000);
+
+        const activationToken = jwt.sign({
+            user,
+            otp,
+        }, 
+        process.env.Activation_Secret,
+        {
+            expiresIn: "5m",
+        }
+    );
+
+    const data = {
+        name,
+        otp,
+    };
+
+    
         
-        res.send("Register Api");
     } catch (error) {
         res.status(300).json({
             message: error.message,
